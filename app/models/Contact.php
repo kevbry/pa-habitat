7d83fa6f0d9b4eab0046724f2a53536ec0548c3a
Chris Stewart <Stewart9240@siast.sk.ca>
2014-10-20 12:39:42 -0600

    Local database test

--- app/models/Contact.php
+++ app/models/Contact.php
@@ -1,8 +1,24 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
 
+class Contact extends Eloquent
+{
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'contact';
+
+	/**
+	 * The attributes that are mass-assignment
+	 *
+	 * @fillable array with column names we wish to be able to assign to.
+	 */
+
+        protected $fillable = array('first_name', 'last_name', 'email');
+}
