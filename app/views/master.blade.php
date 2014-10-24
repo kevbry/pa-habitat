2e27ba3908bdf3840f36ff91ae367176715b4133
Sarah Etter <sarahmichele@gmail.com>
2014-10-24 11:45:43 -0600

    Adding form to add a contact

---
+++ app/views/master.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+@yield('title')
+        </title>
+    </head>
+    <body>
+@yield('content')
+    </body>
+</html>
