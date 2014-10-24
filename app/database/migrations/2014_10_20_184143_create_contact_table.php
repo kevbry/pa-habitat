d6df5711da7304ba235d4252aa7b674b1c2171e5
Chris Stewart <stewart9240@siast.sk.ca>
2014-10-24 10:24:34 -0600

    Completed schema for Contacts Table

--- app/database/migrations/2014_10_20_184143_create_contact_table.php
+++ app/database/migrations/2014_10_20_184143_create_contact_table.php
@@ -3,7 +3,8 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateContactTable extends Migration {
+class CreateContactTable extends Migration 
+{
 
 	/**
 	 * Run the migrations.
