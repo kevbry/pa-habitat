607ae6cfe6e273b7063f1f3484720b2cb0c82c0c
Oseyi Kokobili <kokobili3321@siast.sk.ca>
2014-10-27 11:28:17 -0600

    Added Unit test skeleton

---
+++ app/tests/ViewContacts.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ViewContacts
+ *
+ * @author cst217
+ */
+class ViewContacts {
+    //put your code here
+}
