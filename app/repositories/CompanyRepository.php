da6445dabeef3fc07060fd86161b644d7ed1fc73
Oseyi Kokobili <kokobili3321@siast.sk.ca>
2014-11-21 10:21:15 -0600

    21/11/2014

--- app/repositories/CompanyRepository.php
+++ app/repositories/CompanyRepository.php
@@ -1,21 +1,3 @@
 <?php
 //logic code block
 //set up your code to minimize the amount of php tags nested with html tags
-?>
-
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>habitat-CompanyRepository</title>
-    </head>
-    <body>
-        <h1>habitat-CompanyRepository</h1>
-        <div>
-<?php
-//display code block
-//in these sections add code that displays html on the web page
-//try to minimise nesting php tags within html tags
-?>
-        </div>
-    </body>
-</html>
--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -12,6 +12,7 @@
                 <ul>
                     <li>{{ HTML::linkAction('ContactController@index','All Contacts') }}</li>
                     <li>{{ HTML::linkAction('ContactController@create','Add a Contact') }}</li>
+                    <li>{{ HTML::linkAction('CompanyController@create','Add a Company') }}</li>
                 </ul>
             </nav>
         </header>
