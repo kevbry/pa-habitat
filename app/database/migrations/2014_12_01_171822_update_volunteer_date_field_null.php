41ac2d14875384b1be62d25b49b179096fc263c0
Chris Stewart <stewart9240@siast.sk.ca>
2014-12-03 09:37:03 -0600

    Fixed faulty migration that fixed the last_attended_safety_meeting_date field. Inserted a DB access command that runs SQL to fix the issue.

--- app/database/migrations/2014_12_01_171822_update_volunteer_date_field_null.php
+++ app/database/migrations/2014_12_01_171822_update_volunteer_date_field_null.php
@@ -12,11 +12,23 @@ class UpdateVolunteerDateFieldNull extends Migration {
 	 */
 	public function up()
 	{
-		Schema::table('Volunteer', function($table)
-                {
-                    $table->dropColumn('last_attended_safety_meeting_date');
-                    $table->date('last_attended_safety_meeting_date')->nullable();
-                });
+            // NOTE: This does not appear to work (Migrate throws an error in 
+            // the console).
+            // Instead, we have to run a raw database command as Laravel does 
+            // not support altering existing columns. Keep that in mind - we 
+            // may encounter issues putting restrictions on certain database 
+            // columns we've already made because of this. If someone can get
+            // this to work, that would be great - though this code will 
+            // actually remove any data in this column. The DB command will not
+            // cause data loss.
+            // 
+//		Schema::table('Volunteer', function($table)
+//                {
+//                    $table->dropColumn('last_attended_safety_meeting_date');
+//                    $table->date('last_attended_safety_meeting_date')->nullable();
+//                });
+            
+            DB::statement('ALTER TABLE `habitat_Volunteer` MODIFY `last_attended_safety_meeting_date` DATE NULL;');
 	}
 
 	/**
@@ -26,11 +38,12 @@ class UpdateVolunteerDateFieldNull extends Migration {
 	 */
 	public function down()
 	{
-		Schema::table('Volunteer', function($table)
-                {
-                    $table->dropColumn('last_attended_safety_meeting_date');
-                    $table->date('last_attended_safety_meeting_date');
-                });
+//		Schema::table('Volunteer', function($table)
+//                {
+//                    $table->dropColumn('last_attended_safety_meeting_date');
+//                    $table->date('last_attended_safety_meeting_date');
+//                });
+            DB::statement('ALTER TABLE `habitat_Volunteer` MODIFY `last_attended_safety_meeting_date` DATE NOT NULL;');
 	}
 
 }
