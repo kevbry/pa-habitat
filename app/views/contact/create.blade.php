a1d526c3376b92b75934296aea1d5a742588e4e3
Linden S <lindensoroka@live.com>
2014-12-05 10:45:03 -0600

    Test values added, working demo branch

--- app/views/contact/create.blade.php
+++ app/views/contact/create.blade.php
@@ -12,79 +12,79 @@ Create a Contact
     <div class="form-group">
         {{ Form::label('first_name', 'First Name: ',array('class'=>'col-sm-3')) }}
         <div class="col-sm-7">
-        {{ Form::text('first_name',null,array('class'=>'form-control')) }}
+        {{ Form::text('first_name',"Joe",array('class'=>'form-control')) }}
         </div>
     </div>
     <div class="form-group">
         {{ Form::label('last_name', 'Last Name: ',array('class'=>'col-sm-3')) }}
         <div class="col-sm-7">
-        {{ Form::text('last_name',null,array('class'=>'form-control')) }}
+        {{ Form::text('last_name',"Smith",array('class'=>'form-control')) }}
         </div>
     </div>
     <div class="form-group">
         {{Form::label('company_name', 'Company Name:',array('class'=>'col-sm-3'))}}
         <div class="col-sm-7">
-        {{Form::text('company_name',null,array('class'=>'form-control'))}}
+        {{Form::text('company_name',"Smith's Lumber",array('class'=>'form-control'))}}
         </div>
     </div>
     <div class="form-group">
         {{ Form::label('email_address', 'Email Address: ',array('class'=>'col-sm-3')) }}
         <div class="col-sm-7">
-        {{ Form::input('email','email_address',null,array('class'=>'form-control')) }}
+        {{ Form::input('email','email_address',"Smith@testing.com",array('class'=>'form-control')) }}
         </div>
     </div>
     <div class="form-group">
         {{ Form::label('home_phone', 'Home Phone: ',array('class'=>'col-sm-3')) }}
         <div class="col-sm-7">
-        {{ Form::text('home_phone',null,array('class'=>'form-control')) }}
+        {{ Form::text('home_phone',"306-555-5555",array('class'=>'form-control')) }}
         </div>
     </div>
     <div class="form-group">
         {{ Form::label('cell_phone', 'Cell Phone: ',array('class'=>'col-sm-3')) }}
         <div class="col-sm-7">
-        {{ Form::text('cell_phone',null,array('class'=>'form-control')) }}
+        {{ Form::text('cell_phone',"306-666-6666",array('class'=>'form-control')) }}
         </div>
     </div>
     <div class="form-group">
         {{ Form::label('work_phone', 'Work Phone: ',array('class'=>'col-sm-3')) }}
         <div class="col-sm-7">
-        {{ Form::text('work_phone',null,array('class'=>'form-control')) }}
+        {{ Form::text('work_phone',"306-777-7777",array('class'=>'form-control')) }}
         </div>
     </div>
     <div class="form-group">
         {{ Form::label('street_address', 'Street Address: ',array('class'=>'col-sm-3')) }}
         <div class="col-sm-7">
-        {{ Form::text('street_address',null,array('class'=>'form-control')) }}
+        {{ Form::text('street_address',"123 Main Street",array('class'=>'form-control')) }}
         </div>
     </div>
     <div class="form-group">
         {{ Form::label('city', 'City: ',array('class'=>'col-sm-3')) }}
         <div class="col-sm-7">
-        {{ Form::text('city',null,array('class'=>'form-control')) }}
+        {{ Form::text('city',"Prince Albert",array('class'=>'form-control')) }}
         </div>
     </div>
     <div class="form-group">
         {{ Form::label('province', 'Province: ',array('class'=>'col-sm-3')) }}
         <div class="col-sm-7">
-        {{ Form::text('province',null,array('class'=>'form-control')) }}
+        {{ Form::text('province',"Saskatchewan",array('class'=>'form-control')) }}
         </div>
     </div>
     <div class="form-group">
         {{ Form::label('postal_code', 'Postal Code: ',array('class'=>'col-sm-3')) }}
         <div class="col-sm-7">
-        {{ Form::text('postal_code',null,array('class'=>'form-control')) }}
+        {{ Form::text('postal_code',"S0K 0Z0",array('class'=>'form-control')) }}
         </div>
     </div>
     <div class="form-group">
         {{ Form::label('country', 'Country: ',array('class'=>'col-sm-3')) }}
         <div class="col-sm-7">
-        {{ Form::text('country',null,array('class'=>'form-control')) }}
+        {{ Form::text('country',"Canada",array('class'=>'form-control')) }}
         </div>
     </div>
     <div class="form-group">
         {{ Form::label('comments', 'Comments:',array('class'=>'col-sm-3')) }}
         <div class="col-sm-7">
-        {{ Form::textarea('comments',null,array('class'=>'form-control')) }}
+        {{ Form::textarea('comments',"A hard worker.",array('class'=>'form-control')) }}
         </div>
     </div>
 </section>
