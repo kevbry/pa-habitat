04790884f84558136c147848e8cd79e1cff8ffcd
Alan Schroeder <schroeder9869@siast.sk.ca>
2015-01-14 14:53:48 -0600

    Created Migration and base tests for R3S1

---
+++ app/database/migrations/2015_01_14_202436_fix_volunteer_hours_by_project.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixVolunteerHoursByProject extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+            Schema::table('VolunteeredHours', function($table)
+            {
+                $table->dropColumn('volunteered_hours');
+
+                $table->dropColumn('paid_hours');
+                
+                
+                
+            });
+
+            Schema::table('VolunteeredHours', function($table)
+            {
+                $table->integer('hours')->nullable();
+                
+                $table->boolean('paid_hours');
+                
+                $table->integer('family_id')->nullable();
+            });
+            
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+            Schema::table('VolunteeredHours', function($table)
+            {
+                $table->dropColumn('hours');
+                
+                $table->dropColumn('paid_hours');
+
+                $table->dropColumn('family_id');
+                
+            });
+
+            
+            Schema::table('VolunteeredHours', function($table)
+            {
+                $table->integer('volunteered_hours')->nullable();
+                $table->integer('paid_hours')->nullable();
+            });
+	}
+
+}
---
+++ app/tests/R3S1-Tests/VolunteerHoursTest.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Description of VolunteerHoursTest
+ *
+ * @author cst222
+ */
+class VolunteerHoursTest extends VolunteerHours 
+{
+    protected $mockedVolunteerHoursRepo;
+    protected $mockedVolunteerHoursController;
+    protected $volunteerHoursInput;
+    
+    protected $invalidDataException;
+    
+    public function setUp()
+    {
+        //TODO - Populate array with a dummy entry for the volunteerhours table
+        $this->volunteerHoursInput = [];
+
+        // Set up the Volunteer Hours Mocked Repository
+        $this->mockedVolunteerHoursRepo = Mockery::mock('app\repositories\VolunteerHoursRepository');
+        $this->app->instance('app\repositories\VolunteerHoursRepository', $this->mockedVolunteerHoursRepo);
+        
+        $this->mockedVolunteerHoursController = Mockery::mock('app\controllers\VolunteerHoursController');
+        $this->app->instance('app\controllers\VolunteerHoursController', $this->mockedVolunteerHoursController);
+    }
+    
+    
+    /**
+     * Test that the controller creates and displays the view for adding hours
+     */
+    public function testCreate()
+    {
+        $response = $this->action('GET', 'VolunteerHoursController@create');
+        $crawler = $this->client->request('GET', 'create');
+        
+        $this->assertContains('Add Volunteer Hours',$response->getContent());
+        $this->assertTrue($this->client->getResponse()->isOk());
+        $this->assertCount(1, $crawler->filter('label:contains("Hours:")'));
+    }
+    
+    
+    /**
+     * Test that the controller can sucessfully add hours to the database
+     */
+    public function testStoreSingleEntrySuccess()
+    {
+        // Assemble
+        $this->mockedVolunteerHoursController->shouldReceive('storeHoursEntryWith')->once()->with($this->volunteerHoursInput);
+        $this->mockedVolunteerHoursRepo->shouldReceive('saveVolunteerHours')->once()->with(Mockery::type('VolunteerHours'));
+
+
+        //Redirect::shouldReceive('action')->once()->with('VolunteerHoursController@show');
+
+        // Act 
+        $response = $this->route("POST", "volunteerHours.create", $this->volunteerHoursInput);
+
+        // Assert
+        $this->assertResponseOK();
+        //$this->assertRedirectedToRoute('volunteerHours.create', $response);
+    }
+    
+    public function testStoreSingleEntryFailure()
+    {
+        $this->volunteerHoursInput = [];
+        
+        // Assemble
+        $this->mockedVolunteerHoursController
+                ->shouldReceive('storeHoursEntryWith')
+                ->once()
+                ->with($this->volunteerHoursInput)
+                ->andThrow($this->invalidDataException);
+
+        // Act 
+        $response = $this->route("POST", "volunteerHours.create", $this->volunteerHoursInput);
+
+    }
+    
+}
+
+
