b52e7d0207953e360fc1b66e6aacd405337f3277
Alan Schroeder <schroeder9869@siast.sk.ca>
2015-02-13 09:49:36 -0600

    cleaned up code in master blade template fiel

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -3,27 +3,13 @@
 // Create the master search box located in the nav bar
 $masterSearch = new HabitatSearchBox($PAGE_ROOT_URL, "master", "Search..."); 
 
-
-// Configure the search box
-
-//// Function for setting up the on click method (what happens when a result is selected)
-//$masterSearch->configureOnClickEvent(sprintf(HabitatSearchBox::VIEW_DETAILS_ON_CLICK, $PAGE_ROOT_URL));
-//
-//// Function for setting up how the results are formatted.  First is the attribute to use as the value, second is what to display
-//$masterSearch->configureDatumFormat('id', 'name');
-//
-//// Function for Setting up the selection engine that fetches and formats results from the database
-//$masterSearch->configureEngine('contactSearch', "search/searchContacts?contacts=%QUERY%", 'Contacts');
-//$masterSearch->configureEngine('volunteerSearch', "search/searchVolunteers?volunteers=%QUERY%", 'Volunteers');
-//$masterSearch->configureEngine('projectSearch', "search/searchProjects?projects=%QUERY%", 'Projects');
-//$masterSearch->configureEngine('familySearch', "search/searchFamilies?families=%QUERY%", 'Families');
-//$masterSearch->configureEngine('companySearch', "search/searchCompanies?companies=%QUERY%", 'Companies');
-//
-//// Function for setting up the searchbox settings
-//$masterSearch->configureSettings();
-
-
-// Function for setting up the on click method (what happens when a result is selected)
+/*
+ *  Configure the search box
+ *      Set up how the results are formatted.  First is the attribute to use as the value, second is what to display
+ *      Set up the on click method (what happens when a result is selected)
+ *      Set up the selection engine(s) that fetches and formats results from the database
+ *      Set up the searchbox settings
+ */
 $masterSearch->configureOnClickEvent(sprintf(HabitatSearchBox::VIEW_DETAILS_ON_CLICK, $PAGE_ROOT_URL))
     ->configureDatumFormat('id', 'name')
     ->configureEngine('contactSearch', "search/searchContacts?contacts=%QUERY%", 'Contacts')
