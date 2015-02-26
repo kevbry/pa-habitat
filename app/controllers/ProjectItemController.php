fe215429d15160e6448a557272fcb13581aa3165
Linden S <lindensoroka@live.com>
2015-02-26 14:01:28 -0600

    Commented all the edits (Except project), some javascript, no views yet

--- app/controllers/ProjectItemController.php
+++ app/controllers/ProjectItemController.php
@@ -57,10 +57,14 @@ class ProjectItemController extends \BaseController {
 
         return View::make('projectitem.edit', array('project' => $project, 'itemTypes' => $itemTypes, 'projectItems' => $projectItems));
     }
-    
+    /*
+     * A function to update the items for a project :D
+     */
     public function update() {
+        //Arrays that will contain the items information
         $projectItem = array();
         $infoArray = array();
+        //For every row on the form, add that row to a array containing the rows!
         for ($i = 0; $i < count(Input::get('id')); $i++) {
             //$projectItem['project_id'] = Input::get('project_id');
             $projectItem['id'] = Input::get('id')[$i];
@@ -74,39 +78,54 @@ class ProjectItemController extends \BaseController {
             if (empty($projectItem)) {
                 throw new Exception('No Project Item info inserted.');
             }
+            //Call our helper update method with the row information
             $this->updateItemWith($projectItem);
-            
+            //Add the row to an array so it won't be deleted later
             $infoArray[$i] = $projectItem;
         }
+        //Get the static project id, this never changes and we are making sure it won't
         $id = Input::get('project_id');
-        
+        //Get all the project items from the database, will be used to delete.
         $itemArray = $this->projectItemRepo->getItemsForProjectNonPaginated($id);
+        //IF the database is not empty.
         if(!empty($itemArray))
         {
+            //For every item in the database.
             foreach($itemArray as $itemEntry)
             {
+                //Haven't found an item to keep yet.
                 $bFound = false;
+                //If the rows aren't empty on the form.
                 if(!empty($infoArray))
                 {
+                    //For every row on the form.
                     foreach($infoArray as $formEntry)
                     {
+                        //Is it the same as one in the database?
                         if( strval($itemEntry['id']) == $formEntry['id'] )
                         {
+                            //If it is, we are keeping it.
                             $bFound = true;
                         }
                     }
                 }
+                //Row in the database doesn't exist on the form.
                 if(!$bFound)
                 {
+                    //So we nuke it out of the database as well.
                     $affectedRows = ProjectItem::where('id','=',$itemEntry['id'])->delete();
                 }
             }     
         }
+        //Redirect back to index for project items
         return Redirect::action('ProjectItemController@index', $id);
     }
-    
+    /*
+     * Helper method to update item row in the database
+     */
     public function updateItemWith($projectItem) {
         $counter = 0;
+        //Generic array of database field names.
         $fieldNames = array(
             //'project_id',
             'id', 
@@ -117,18 +136,26 @@ class ProjectItemController extends \BaseController {
             'vendor',
             'comments'
         );
+        //Array to have the keys/values to update the row.
         $fieldUpdateValues = array();
+        //For every value passed from the form entry.
         foreach($projectItem as $fieldValue)
         {
+            //Don't update the first one, it's an id to be used lower as a primary key.
             if($counter != 0)
             {
+                //Add the key ($fieldNames[$counter]) and the value ($fieldValue)
+                //To an array ($fieldUpdateValues) to be updated lower.
                 $fieldUpdateValues = array_add($fieldUpdateValues, $fieldNames[$counter], $fieldValue);
             }
             $counter++; 
         }
+        //Update the field/values in $fieldUpdateValues!!!
         $affectedRows = ProjectItem::where('id','=',$projectItem['id'])->update($fieldUpdateValues);
     }
-
+    /*
+     * A function to store an item.
+     */
     public function storeItemWith($projectItem) {
 
         $item = new ProjectItem($projectItem);
--- app/controllers/VolunteerHoursController.php
+++ app/controllers/VolunteerHoursController.php
@@ -67,12 +67,13 @@ class VolunteerHoursController extends \BaseController {
     }
 
     public function indexForEditContact($contactId) {
+        //Created our own method to call the specific view.
         $volunteer = $this->volunteerRepo->getVolunteer($contactId);
         $projects = $this->projectRepo->getAllProjectsNonPaginated();
         $volunteerHours = $this->volunteerHrsRepo->getHoursForVolunteerNonPaginated($contactId);
        
         $families = $this->familyRepo->getAllFamiliesNonPaginated();
-       
+       //Create view and pass in necessary parameters.
         return View::make('volunteerhours.volunteerEdit', array('id' => $contactId, 'volunteer' => $volunteer,
                     'projects' => $projects, 'volunteerhours' => $volunteerHours,
                     'families' => $families));
@@ -123,11 +124,14 @@ $type=Input::get('pageType');
 
         return $hours->id;
     }
-    
+    //Method for getting the inputs from the volunteeredit view.
     public function updatehours()
     {
+        //Create two arrays that will be used to delete/update
         $infoArray = array();
         $hoursInfo = array();
+        //Fill an array with the inputs from the page, have to loop as they are
+        //arrays
         for ($i = 0; $i < count(Input::get('row_id')); $i++) {
             $hoursInfo['id'] = Input::get('row_id')[$i];
             $hoursInfo['volunteer_id'] = Input::get('volunteer_id')[$i];
@@ -146,48 +150,56 @@ $type=Input::get('pageType');
             if (empty($hoursInfo)) {
                 throw new Exception('No Hours info inserted.');
             }
+            //Add the specific hour row information into the $infoArray.
             $infoArray[$i] = $hoursInfo;
+            //Call our helper method on the row, to update it.
+            $this->updateHoursWith($hoursInfo);
         }
-        $volID = Input::get('vol_id');
-        $id = $volID;
+        //Get a form field that gets the volunteers id that we are editing.
+        //For safety sake made anothing variable that won't change on inputs.
+        $id = Input::get('vol_id');
         
-        if(!empty($infoArray))
-        {
-            for($i = 0; $i < count($infoArray); $i++)
-            {
-                $this->updateHoursWith($infoArray[$i]);
-            }
-        }
-        
+        //Get all the hours for the volunteer, will need this to delete.
         $hourArray = $this->volunteerHrsRepo->getHoursForVolunteerNonPaginated($id);
-
+        
+        //For every hour entry in the volunteers database.
         foreach($hourArray as $hourEntry)
         {
+            //The hour hasn't been found yet.
             $bFound = false;
+            //$infoArray is the inputs
+            //$hourArray is the database
+            //If the input array wasn't empty.
             if(!empty($infoArray))
             {
                 foreach($infoArray as $formEntry)
                 {
+                    //if the input row got deleted from the page
                     if( strval($hourEntry['id']) == $formEntry['id'] )
                     {
+                        //We found a row that should exist
                         $bFound = true;
                     }
                 }
             }
-            
+            //If the row didn't exist on the page, but did in the database.
             if(!$bFound)
             {
+                //Delete the row from the database.
                 $affectedRows = VolunteerHours::where('id','=',$hourEntry['id'])->delete();
             }
-
         }
-
+        //Redirect to the contacts hour page.
         return Redirect::action('VolunteerHoursController@indexForContact', $id);
-        
     }
+    //Helper method to update a row in the database
+    /*
+     * $hoursInfo = the row passed in to update.
+     */
     public function updateHoursWith($hoursInfo)
     {
         $counter = 0;
+        //An array of the database field names.
         $fieldNames = array(
             'id',
             'volunteer_id',
@@ -197,24 +209,33 @@ $type=Input::get('pageType');
             'paid_hours',
             'family_id'
         );
+        //An array of field->values to be populated to be updated.
         $fieldUpdateValues = array();
+        //For every field in $hoursInfo.
         foreach($hoursInfo as $fieldValue)
         {
+            //Don't update the first one as that's just an id that is needed as a key lower.
             if($counter != 0)
             {
+                //Add the key ($fieldNames[$counter]) with the value $fieldValue
+                //To an array of field->values
                 $fieldUpdateValues = array_add($fieldUpdateValues, $fieldNames[$counter], $fieldValue);
             }
             $counter++; 
         }
+        //Update the row with the array of field->values
         $affectedRows = VolunteerHours::where('id','=',$hoursInfo['id'])->update($fieldUpdateValues);
     }
-    
+    /*
+     * Function to generate an hours report for a volunteer
+     */
     public function viewHoursReport($id)
     {
         $volunteer = $this->volunteerRepo->getVolunteer($id);
         $projects = $this->projectRepo->getAllProjects();
         $volunteerHours = $this->volunteerHrsRepo->getHoursForVolunteerSortedByProject($id);
         
+        //Count the total hours for the volunteer.
         $totalHours = 0;
         if(!empty($volunteerHours))
         {
@@ -225,18 +246,21 @@ $type=Input::get('pageType');
         }
        
         $families = $this->familyRepo->getAllFamiliesNonPaginated();
-       
+        //Make the volunteer hour report view.
         return View::make('report.volunteer', array('id' => $id, 'volunteer' => $volunteer,
                     'projects' => $projects, 'volunteerhours' => $volunteerHours,
                     'families' => $families, 'totalHours' => $totalHours));
     }
-    
+    /*
+     * Generate a hours report for project
+     */
     public function viewHoursReportForProject($id)
     {
         $project = $this->projectRepo->getProject($id);
         $volunteers = $this->volunteerRepo->getAllVolunteers();
         $volunteerHours = $this->volunteerHrsRepo->getHoursForProjectSortedByVolunteer($id);
         
+        //Get the total hours for the project.
         $totalHours = 0;
         if(!empty($volunteerHours) && count($volunteerHours) != 0)
         {
@@ -247,31 +271,39 @@ $type=Input::get('pageType');
         }
        
         $families = $this->familyRepo->getAllFamiliesNonPaginated();
-       
+        //Make the project hours view.
         return View::make('report.project', array('id' => $id, 'project' => $project,
                     'volunteers' => $volunteers, 'volunteerhours' => $volunteerHours,
                     'families' => $families, 'totalHours' => $totalHours));
-
     }
+    /*
+     * function that builds the edit project hours view.
+     */
     public function indexForEditProject($projectId) {
         $volunteers = $this->volunteerRepo->getAllVolunteersNonPaginated();
         $project = $this->projectRepo->getProject($projectId);
         $volunteerHours = $this->volunteerHrsRepo->getHoursForProject($projectId);
         $projects = $this->projectRepo->getAllProjectsNonPaginated();
-        //MAKE NON-PAGINATEd, REMEMBER TO REMOVE LINKS AT BOTTOM OF THE VIEW.
        
         $families = $this->familyRepo->getAllFamiliesNonPaginated();
-       
+       //Generate the edit project hours view.
         return View::make('volunteerhours.projectedit', array('id' => $projectId, 'volunteers' => $volunteers,
                     'project' => $project, 'projects' => $projects, 'volunteerhours' => $volunteerHours,
                     'families' => $families));
     }
-    
+    /*
+     * Function that takes the inputs of the page, and passes them to our update helper method
+     * Also deletes any rows that were not found on the page.
+     */
     public function updateProjectHours()
     {
+        //Two arrays that will hold hour information later.
         $infoArray = array();
         $hoursInfo = array();
+        //The static projectID that doesn't change on the page.
         $projectId = Input::get('proj_id');
+        //For every field on the page, populate the hoursInfo array
+        //With the values form the page.
         for ($i = 0; $i < count(Input::get('row_id')); $i++) {
             $hoursInfo['id']= Input::get('row_id')[$i];
             $hoursInfo['project_id'] = Input::get('project_id')[$i];
@@ -290,40 +322,47 @@ $type=Input::get('pageType');
             if (empty($hoursInfo)) {
                 throw new Exception('No Hours info inserted.');
             }
+            //Add the hoursInfo data to the infoArray
             $infoArray[$i] = $hoursInfo;
-
+            //Calls our helper method to update the hour row, right below.
             $this->projectUpdateHoursWith($hoursInfo);
         }
+        //Get the hours for the project from the database.
         $hourArray = $this->volunteerHrsRepo->getHoursForProjectNonPaginated($projectId);
-
+        //For every database row of hours
         foreach($hourArray as $hourEntry)
         {
+            //Haven't found a row to keep.
             $bFound = false;
+            //If the array isn't empty.
             if(!empty($infoArray))
             {
+                //For every row in the form.
                 foreach($infoArray as $formEntry)
                 {
+                    //If the row exists on the form and the database
                     if( strval($hourEntry['id']) == $formEntry['id'] )
                     {
+                        //We found a row to keep.
                         $bFound = true;
                     }
                 }
             }
-            
+            //If we didn't find a row to keep (Didn't exist on the form)
             if(!$bFound)
             {
+                //Nuke that row from the database.
                 $affectedRows = VolunteerHours::where('id','=',$hourEntry['id'])->delete();
             }
-
         }
-       
+        //Redirect back to the project page.
         return Redirect::action('VolunteerHoursController@indexForProject', $projectId);
-        
     }
-    
+    //Helper method to update a single row in the database.
     public function projectUpdateHoursWith($hoursInfo)
     {
         $counter = 0;
+        //Create an array with the key fields of the database.
         $fieldNames = array(
             'id',
             'project_id',
@@ -333,15 +372,21 @@ $type=Input::get('pageType');
             'paid_hours',
             'family_id'
         );
+        //Create an empty array that will be used to update the row.
         $fieldUpdateValues = array();
+        //For field that was passed in.
         foreach($hoursInfo as $fieldValue)
         {
+            //The first one is just an id used below as a primary key. Not updating it.
             if($counter != 0)
             {
+                //Add the key ($fieldNames[$counter]) and the value ($fieldValue)
+                //To the array $fieldUpdateValues.
                 $fieldUpdateValues = array_add($fieldUpdateValues, $fieldNames[$counter], $fieldValue);
             }
             $counter++; 
         }
+        //Update the row that $fieldUpdateValues contains the info for.
         $affectedRows = VolunteerHours::where('id','=',$hoursInfo['id'])->update($fieldUpdateValues);
     }
 }
