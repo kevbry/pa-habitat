8ba5dbd4a40357049ac51aa84fbd8299eed227c3
Oseyi Kokobili <kokobili3321@siast.sk.ca>
2015-03-11 14:50:46 -0600

    Added unit tests

--- app/controllers/ProjectInspectionController.php
+++ app/controllers/ProjectInspectionController.php
@@ -71,5 +71,38 @@ class ProjectInspectionController extends \BaseController {
         $this->projectInspectionRepo->saveProjectInspection($inspection);
 
         return $inspection->id;
-    }   
+    }
+        /*
+     * Helper method to update item row in the database
+     */
+    public function updateInspectionWith($projectInspection) {
+        $counter = 0;
+        //Generic array of database field names.
+        $fieldNames = array(
+            //'project_id',
+            'id', 
+            'project_id', 
+            'mandatory', 
+            'date',
+            'type',
+            'pass',
+            'comments'
+        );
+        //Array to have the keys/values to update the row.
+        $fieldUpdateValues = array();
+        //For every value passed from the form entry.
+        foreach($projectInspection as $fieldValue)
+        {
+            //Don't update the first one, it's an id to be used lower as a primary key.
+            if($counter != 0)
+            {
+                //Add the key ($fieldNames[$counter]) and the value ($fieldValue)
+                //To an array ($fieldUpdateValues) to be updated lower.
+                $fieldUpdateValues = array_add($fieldUpdateValues, $fieldNames[$counter], $fieldValue);
+            }
+            $counter++; 
+        }
+        //Update the field/values in $fieldUpdateValues!!!
+        $affectedRows = ProjectInspection::where('id','=',$projectInspection['id'])->update($fieldUpdateValues);
+    }
 }
