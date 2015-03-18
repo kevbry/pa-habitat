256f22d6305e136898bd7f5acda49004c18e563d
Linden S <lindensoroka@live.com>
2015-03-18 13:26:16 -0600

    Added a working file, wasn't working on some

--- app/libraries/ValidatorRules/CellPhoneValidatorRule.php
+++ app/libraries/ValidatorRules/CellPhoneValidatorRule.php
@@ -1,24 +1,18 @@
 <?php
 
-/**
- * Description of PhoneValidationRule
- * Custom validator for phone numbers.
- * @author cst224
- */
-//IF YOU ARE CREATING CUSTOM VALIDATORS, MAKE SURE TO ADD A METHOD SIMILAR TO EXISTING
-//METHODS IN THE BOTTOM OF THE APP/START/GLOBAL.PHP FILE, ELSE IT WILL THINK IT DOESN"T EXIST
-class PhoneValidatorRule extends \Illuminate\Validation\Validator {
+
+class CellPhoneValidatorRule extends \Illuminate\Validation\Validator {
     //Create the custom validation, this is a regex for phone.
-    public function validatePhone($attribute, $value, $parameters)
+    public function validateCellPhone($attribute, $value, $parameters)
     {
         return preg_match("^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$^", $value);
     }
     //This method replaces the terrible validation.phone error message with a beautiful informative one.
     //You should probably mention the field names in lowercase, because this is how we use assoc arrays
     //to actually display the messages. It might break if you use upper case.
-    protected function replacePhone($message, $attribute, $rule, $parameters, $phone='')
+    protected function replaceCellPhone($message, $attribute, $rule, $parameters, $phone='')
     {
-         return str_replace('validation.phone', 'The home phone field should be in the format ###-###-####', $message);
+         return str_replace('validation.CellPhone', 'The cell phone field should be in the format ###-###-####', $message);
     }
  //*****************READ THIS*******************************//
     /*
@@ -26,5 +20,4 @@ class PhoneValidatorRule extends \Illuminate\Validation\Validator {
      * IS ADDED AT THE BOTTOM OF START/GLOBAL.php FOR THE METHOD OF YOUR VALIDATION.
      */
 }
-
-Validator::extend('phone_rule', 'PhoneValidatorRule@validatePhone');
+Validator::extend('cellphone', 'CellPhoneValidatorRule@validateCellPhone');
--- app/libraries/ValidatorRules/WorkPhoneValidatorRule.php
+++ app/libraries/ValidatorRules/WorkPhoneValidatorRule.php
@@ -1,24 +1,17 @@
 <?php
 
-/**
- * Description of PhoneValidationRule
- * Custom validator for phone numbers.
- * @author cst224
- */
-//IF YOU ARE CREATING CUSTOM VALIDATORS, MAKE SURE TO ADD A METHOD SIMILAR TO EXISTING
-//METHODS IN THE BOTTOM OF THE APP/START/GLOBAL.PHP FILE, ELSE IT WILL THINK IT DOESN"T EXIST
-class PhoneValidatorRule extends \Illuminate\Validation\Validator {
+class WorkPhoneValidatorRule extends \Illuminate\Validation\Validator {
     //Create the custom validation, this is a regex for phone.
-    public function validatePhone($attribute, $value, $parameters)
+    public function validateWorkPhone($attribute, $value, $parameters)
     {
         return preg_match("^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$^", $value);
     }
     //This method replaces the terrible validation.phone error message with a beautiful informative one.
     //You should probably mention the field names in lowercase, because this is how we use assoc arrays
     //to actually display the messages. It might break if you use upper case.
-    protected function replacePhone($message, $attribute, $rule, $parameters, $phone='')
+    protected function replaceWorkPhone($message, $attribute, $rule, $parameters, $phone='')
     {
-         return str_replace('validation.phone', 'The home phone field should be in the format ###-###-####', $message);
+         return str_replace('validation.WorkPhone', 'The work phone field should be in the format ###-###-####', $message);
     }
  //*****************READ THIS*******************************//
     /*
@@ -26,5 +19,4 @@ class PhoneValidatorRule extends \Illuminate\Validation\Validator {
      * IS ADDED AT THE BOTTOM OF START/GLOBAL.php FOR THE METHOD OF YOUR VALIDATION.
      */
 }
-
-Validator::extend('phone_rule', 'PhoneValidatorRule@validatePhone');
+Validator::extend('workphone', 'WorkPhoneValidatorRule@validateWorkPhone');
