15de4f8da6e8f762508329130b4fa4fad5271066
Sarah Etter <etter1596@siast.sk.ca>
2015-04-01 14:10:03 -0600

    Changing user ids

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -15,7 +15,7 @@ class UserTableSeeder extends Seeder
         DB::table('User')->delete();
         
         User::create([
-            'contact_id' => $contacts[0],
+            'contact_id' => $contacts[0]->id,
             'password'  => Hash::make('password'),
             'username' => 'admin',
             'access_level' => 'administrator'
