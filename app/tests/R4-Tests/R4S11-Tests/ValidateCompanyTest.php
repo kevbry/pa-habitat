16bbfa4d766bdb7d1a8b4a37438c5b8b6e3cf050
Wyatt Anderson <anderson5079@siast.sk.ca>
2015-04-07 13:49:29 -0600

    added unit tests for company info verification

---
+++ app/tests/R4-Tests/R4S11-Tests/ValidateCompanyTest.php
@@ -0,0 +1,56 @@
+<?php
+ 
+use Mockery as m;
+use App\Libraries\validators\CompanyValidator;
+ 
+//This will create a volunteer and fill out every field, including the last 
+//attended safety meeting date. The tests are then run on everything.
+class ValidateCompanyTest extends TestCase {
+
+    protected $goodCompanyInfo;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->goodCompanyInfo = array(
+             'name' => 'Yen Company',
+             'primary_contact_1' => '1',
+            );
+    }
+    public function testValidatorPassesAll()
+     {
+         $companyValidator = new App\Libraries\validators\CompanyValidator($this->goodCompanyInfo);
+
+         $this->assertTrue($companyValidator->passes());
+     }
+     public function testValidateNamePass()
+    {
+        $this->goodCompanyInfo["name"] = "name";
+        $companyValidator = new App\Libraries\validators\CompanyValidator($this->goodCompanyInfo);
+
+        $this->assertTrue($companyValidator->passes());
+    }
+    public function testValidateNameFail()
+    {
+        $this->goodCompanyInfo["name"] = "";
+        $companyValidator = new App\Libraries\validators\CompanyValidator($this->goodCompanyInfo);
+
+        $this->assertFalse($companyValidator->passes());
+    }
+    
+    public function testValidatePrimaryContact1Pass()
+    {
+        $this->goodCompanyInfo["primary_contact_1"] = "1";
+        $companyValidator = new App\Libraries\validators\CompanyValidator($this->goodCompanyInfo);
+
+        $this->assertTrue($companyValidator->passes());
+    }
+    public function testValidatePrimaryContact1Fail()
+    {
+        $this->goodCompanyInfo["primary_contact_1"] = "";
+        $companyValidator = new App\Libraries\validators\CompanyValidator($this->goodCompanyInfo);
+
+        $this->assertFalse($companyValidator->passes());
+    }
+}    
