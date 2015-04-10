c9e1f6f0506722f8959742598975b11316d76502
Wyatt Anderson <anderson5079@siast.sk.ca>
2015-04-10 09:57:59 -0600

    added appropriate message and user access level check

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -6,6 +6,10 @@ Home
 
 @section('content')
 <h2>Welcome {{Session::get('username')}} to the Habitat for Humanity Contact and Project Management System</h2>
+@if (Auth::check() && (Session::get('access_level') === 'inactive' ))
+<p>Your account has been set to inactive. Please contact an administrator.</p>
+@else
 <p>Please select an option from the navigation bar above.</p>
+@endif
 
 @stop            
